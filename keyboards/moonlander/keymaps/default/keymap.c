5576603472d9742dff78d95609bd81fae456e426
Jonathan Siemering <git@siemering.eu>
2023-08-10 22:46:08 +0200

    Add browser forward and backword and backslash

--- keyboards/moonlander/keymaps/default/keymap.c
+++ keyboards/moonlander/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_CAPS,          KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
-        _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_DEL,           KC_PGDN, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC,
+        KC_BSLS, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_DEL,           KC_PGDN, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC,
         KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_EQL,           KC_MINS, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT, WIN_MGR,                  MO(MOUSE),KC_UP,   KC_DOWN, KC_RALT, KC_RGUI, KC_RCTL,
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
         _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-        EE_CLR,  _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
+        EE_CLR,  _______, _______, KC_WBAK, KC_WFWD,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
                                             RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,_______, _______
     ),
 
