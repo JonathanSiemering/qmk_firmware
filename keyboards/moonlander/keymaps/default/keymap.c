c24b516cf063d9573c132a8d16650d51bcd933da
Jonathan Siemering <git@siemering.eu>
2023-08-14 08:44:07 +0200

    Fix mac layer

--- keyboards/moonlander/keymaps/default/keymap.c
+++ keyboards/moonlander/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ bool is_rsft_pressed = false;
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    MO(SETTING),     KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    MO(SETTING),      KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
         KC_BSLS, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_DEL,           KC_PGDN, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC,
         KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_EQL,           KC_MINS, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
-        _______, KC_LALT, KC_LGUI, _______, _______,          _______,           _______,          _______, KC_RGUI, KC_RALT, _______, _______,
+        _______, KC_LALT, KC_LGUI, _______, _______,          _______,           _______, _______, _______, KC_RGUI, KC_RALT, _______,
                                             _______, _______, _______,           _______, _______, _______
     ),
 
