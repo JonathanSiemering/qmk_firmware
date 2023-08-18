44441e474f695590da9cb03caae810a4a4b7e82e
Jonathan Siemering <git@siemering.eu>
2023-08-18 23:54:01 +0200

    Disable colemak dafault

--- keyboards/keychron/q1/rev_0100/keymaps/default/keymap.c
+++ keyboards/keychron/q1/rev_0100/keymaps/default/keymap.c
@@ -210,9 +210,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-void keyboard_post_init_user(void) {
-    layer_on(ALL_CK);
-}
+// void keyboard_post_init_user(void) {
+//     layer_on(ALL_CK);
+// }
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
